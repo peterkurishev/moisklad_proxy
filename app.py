3d037cbc44be3424400ed29736c730533ad5eba3
Peter A. Kurishev <peter.kurishev@gmail.com>
2020-11-17 02:44:14 +0300

    pep8ify the code

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 '''Moi sklad proxy to control the permissions'''
-from flask import Flask,request,redirect,Response
+from flask import Flask, request, redirect, Response
 import requests
 
 app = Flask(__name__)
 PROXIED_API = 'https://online.moysklad.ru'
 
+
 @app.route('/')
 def index():
     return 'Flask is running!'
 
-@app.route('/<path:path>',methods=['GET','POST','DELETE'])
+
+@app.route('/<path:path>', methods=['GET', 'POST', 'DELETE'])
 def proxy(path):
     '''Main proxying method'''
     response = None
-    if request.method=='GET':
+    if request.method == 'GET':
         resp = requests.get(f'{PROXIED_API}{path}')
         excluded_headers = ['content-encoding', 'content-length',
                             'transfer-encoding', 'connection']
@@ -21,19 +23,20 @@ def proxy(path):
                    if name.lower() not in excluded_headers]
         response = Response(resp.content, resp.status_code, headers)
 
-    elif request.method=='POST':
-        resp = requests.post(f'{PROXIED_API}{path}',json=request.get_json())
+    elif request.method == 'POST':
+        resp = requests.post(f'{PROXIED_API}{path}', json=request.get_json())
         excluded_headers = ['content-encoding', 'content-length',
                             'transfer-encoding', 'connection']
         headers = [(name, value) for (name, value) in resp.raw.headers.items()
                    if name.lower() not in excluded_headers]
         response = Response(resp.content, resp.status_code, headers)
 
-    elif request.method=='DELETE':
+    elif request.method == 'DELETE':
         resp = requests.delete(f'{PROXIED_API}{path}').content
         response = Response(resp.content, resp.status_code, headers)
 
     return response
 
+
 if __name__ == '__main__':
-    app.run(debug = False,port=80)
+    app.run(debug=False, port=80)
