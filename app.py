9fa3fde1c0b34fc545adb350e6e7c0e29010e6ad
Peter A. Kurishev <peter.kurishev@gmail.com>
2020-11-20 04:22:09 +0300

    add Flask admin interface
    Warning: should be closed with Nginx basic auth

--- app.py
+++ app.py
@@ -74,12 +74,6 @@ class LogView(ModelView):
 class UserView(ModelView):
     column_list = ['id', 'name', 'password_hash']
 
-    def is_accessible(self):
-        return login.current_user.is_authenticated
-
-    def inaccessible_callback(self, name, **kwargs):
-        # redirect to login page if user doesn't have access
-        return redirect(url_for('login', next=request.url))
 
 admin.add_view(UserView(UserAuth, db.session))
 admin.add_view(LogView(LogItems, db.session))
